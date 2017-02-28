d3727f0302f06a184f1bfce9bafb6080928b5384
bgb0923 <bgb0923@gmail.com>
2017-02-28 17:05:30 -0500

    Done

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,15 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var jonBonJovi: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        constrainJonBonJovi()
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        
-
+        UIView.animateKeyframes(withDuration: 3, delay: 0, options: .calculationModeLinear, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/5, animations: { _ in
+                self.jonBonJovi.transform = CGAffineTransform(scaleX: 1, y: 0.9)
+            })
+            UIView.addKeyframe(withRelativeStartTime: 1/5, relativeDuration: 1/5, animations: { _ in
+                self.jonBonJovi.transform = CGAffineTransform(scaleX: 1, y: 3)
+            })
+            UIView.addKeyframe(withRelativeStartTime: 2/5, relativeDuration: 1/5, animations: { _ in
+                self.jonBonJovi.transform = CGAffineTransform(scaleX: 1, y: 0.9)
+            })
+            UIView.addKeyframe(withRelativeStartTime: 3/5, relativeDuration: 1/5, animations: { _ in
+                self.jonBonJovi.transform = CGAffineTransform(scaleX: 1, y: 1)
+            })
+        }, completion: nil)
     }
 
+    func constrainJonBonJovi() {
+        self.jonBonJovi.translatesAutoresizingMaskIntoConstraints = false
+        self.jonBonJovi.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    
+    
+    
 }
 
